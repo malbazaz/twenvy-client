d0fa33c6c865c7c46db369bda459df6ba4da54fe
malbazaz <albazaz@gmail.com>
2018-10-24 16:47:27 -0400

    Update Products container and move the Campaigns Form to components

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ProductCard from '../components/productCard'
+import {connect} from 'react-redux'
 
-const Products = (props) => (
-    <div>
-        <h3>Product Components</h3>
-        {props.products.map(product => 
+class Products extends Component{ 
+
+ 
+   render(){
+       return(
         <div>
-        <h1>{product.name}</h1>
-        <p>Model: {product.model} </p>
-        </div>
-        )}
+        <h3>Product Components</h3>
+        {this.props.products.map(product => <productCard key={product.id} product={product} />)}
+        <productForm />
     </div>
-)
+       )
+   } 
+}
 
-export default Products;
+const mapStateToProps = (state) => {
+    return({
+        products: this.state.products
+    })
+}
+export default connect(mapStateToProps)(products); 
