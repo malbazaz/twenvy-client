c4a16831ff16287783bc7a2d1d6e24e44871dbae
malbazaz <albazaz@gmail.com>
2018-10-24 15:00:10 -0400

    Import reducers to store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
+import productsReducer from '../reducers/productsReducer';
+import campaignsReducer from '../reducers/campaignsReducer';
 
 
 export default createStore(
