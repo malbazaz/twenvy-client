5425a0fa11277b7e7d64704bdfb417c5be407303
malbazaz <albazaz@gmail.com>
2018-10-24 13:05:08 -0400

    Add test reducer for campaign

--- src/store.js
+++ src/store.js
@@ -1,2 +1,13 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
+
+const campaignsReducer = (states = [], action) =>{
+    switch(action.type) {
+        case 'START_CAMPAIGN':
+            return action.start
+        default:
+        return state;
+    }
+}
+
+const campaigns = campaignsReducer(undefined, {type: '@@INIT'});
