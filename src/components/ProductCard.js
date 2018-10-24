b0919389f1334c85ecab1dc5e67885ebeb97b173
malbazaz <albazaz@gmail.com>
2018-10-24 16:48:45 -0400

    Create and add ProductCard

---
+++ src/components/ProductCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProductCard = ({product}) => (
+<div key={product.id} className="ProductCard">
+<h3>Product Id: {product.product_id}</h3>
+    <h3>End date: {product.end_date}</h3>
+    <h3>location: {product.location}</h3>
+    <h3>Target Qty: {product.target_qty}</h3>
+    <h3>Sold Qty: {product.sold_qty}</h3>
+</div>
+
+)
+
+export default ProductCard
