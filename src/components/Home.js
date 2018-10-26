ca2813db3167f90517cb485106a5a176c0475b8e
malbazaz <albazaz@gmail.com>
2018-10-25 22:10:49 -0400

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CampaignCard = () => (
+<div className="Home">
+<h2>Hi, Welcome to the Campaign App</h2>
+<h2>To checkout the products available to start your crowdsourcing campaign, click on Products</h2>
+<h2>You can also view existing Campaigns and start your own Campaign</h2>
+<h2>You can also join an existing Campaign!!</h2>
+</div>
+
+)
+
+export default CampaignCard
