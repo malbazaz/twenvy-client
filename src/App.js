4bc677fe4586a3d41a896ade638318c9a0d9467a
malbazaz <albazaz@gmail.com>
2018-10-25 22:15:30 -0400

    Clean App.js

--- src/App.js
+++ src/App.js
@@ -11,32 +11,10 @@ import CampaignForm from './containers/CampaignForm'
 import productsReducer from './reducers/productsReducer'
 import campaignsReducer from './reducers/campaignsReducer'
 
-// const products = [{
-//   name: "Iphone XS",
-//   brand: "Apple",
-//   price: 999,
-//   img_url: "",
-//   description: "Coolest Iphone ever.",
-//   category_id: 1, 
-//   model: "iphoneXS_V01"
-// }]
-// const campaigns = [{
-//   product_id: 2, 
-//   end_date: 2018-12-10, 
-//   location: "San Francisco",
-//   target_qty: 20, 
-//   sold_qty:10
-// }]
 
 
 class App extends Component {
-  // constructor(props){
-  //   super(props)
-  //   this.state={
-  //     campaigns:[], 
-  //     products: []
-  //   }
-  // }
+
   componentDidMount() {
     this.props.fetchCampaigns() 
     // this.props.fetchProducts()
