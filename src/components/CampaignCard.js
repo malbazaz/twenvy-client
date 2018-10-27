12a48754ada0d235fcb8b6b92d6cf5104c642348
malbazaz <albazaz@gmail.com>
2018-10-27 13:07:57 -0400

    Add ProgressBar and beautify further CampaignCard.

--- src/components/CampaignCard.js
+++ src/components/CampaignCard.js
@@ -2,14 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Row, Col, CardHeader } from 'reactstrap'
+import {ProgressBar} from 'react-bootstrap'
 
 
 const CampaignCard = ({campaign}) => (
 <div key={campaign.id} className="CampaignCard">
 <Row>
 <Col sm="6">
-<Card>
-<CardHeader><Link to={`/campaigns/${campaign.id}`}> Campaign Number: {campaign.id}</Link></CardHeader>
+<Card body outline color="primary">
+<CardHeader inverse color="primary"><Link to={`/campaigns/${campaign.id}`}> Campaign Number: {campaign.id}</Link></CardHeader>
     <CardBody>
 <CardTitle>Product Name: </CardTitle>
     <CardSubtitle>Product Brand: {campaign.product_id}</CardSubtitle>
@@ -18,6 +19,7 @@ const CampaignCard = ({campaign}) => (
     <CardText>location: {campaign.location}</CardText>
     <CardText>Target Qty: {campaign.target_qty}</CardText>
     <CardText>Sold Qty: {campaign.sold_qty}</CardText>
+    <ProgressBar active now={(campaign.sold_qty/campaign.target_qty)*100} label={`${(campaign.sold_qty/campaign.target_qty)*100}%`} />
     </CardBody>
 </Card>
 </Col>
