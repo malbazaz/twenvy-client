a7d5debc0e02c0f2502c458a8d2b816c18a5f6b7
malbazaz <albazaz@gmail.com>
2018-10-29 12:19:34 -0400

    Beautify home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const CampaignCard = () => (
-<div className="Home">
-<h2>Hi, Welcome to the Campaign App</h2>
+<div className="Home" class="center-div">
+<h2><strong>Welcome to the Campaign App</strong></h2>
+<br/>
 <h2>To checkout the products available to start your crowdsourcing campaign, click on Products</h2>
 <h2>You can also view existing Campaigns and start your own Campaign</h2>
-<h2>You can also join an existing Campaign!!</h2>
+<h2>You can also search for and join an existing Campaign!!</h2>
 </div>
 
 )
