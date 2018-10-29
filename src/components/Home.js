f345ffd5d30f70884f5fb6e110d2262cd9fdc151
malbazaz <albazaz@gmail.com>
2018-10-29 12:20:48 -0400

    Change color for home page to blue.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const CampaignCard = () => (
 <div className="Home" class="center-div">
+<font color="#0000A0">
 <h2><strong>Welcome to the Campaign App</strong></h2>
 <br/>
 <h2>To checkout the products available to start your crowdsourcing campaign, click on Products</h2>
 <h2>You can also view existing Campaigns and start your own Campaign</h2>
 <h2>You can also search for and join an existing Campaign!!</h2>
+</font>
 </div>
 
 )
